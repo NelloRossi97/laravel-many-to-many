1902b751134faefe6f8244717be4dc93df8d261e
NelloRossi <rossianiello97@gmail.com>
2023-06-08 15:56:18 +0200

    made relations to table tag and project

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -15,4 +15,9 @@ class Project extends Model
     {
         return $this->belongsTo(Type::class);
     }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
 }
